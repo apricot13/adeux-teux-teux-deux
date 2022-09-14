632e46c2350ff4d2ceda676e24ccc536bad049ea
Hannah <xhan104@gmail.com>
2022-09-14 13:17:13 +0100

    updating cli parts

--- cli/list-tasks.js
+++ cli/list-tasks.js
@@ -1,17 +1,16 @@
 const puppeteer = require("puppeteer")
 const moment = require("moment")
 const fs = require("fs")
+const Table = require('tty-table')
 require('dotenv').config()
-
 const url = 'https://teuxdeux.com/home';
 const userDataDir = './../data/puppeteer_data';
 const userSession = './../data/session.json';
-const taskText = process.argv[2];
-const showDate = true; //todo
+
+// chalk docs https://github.com/chalk/chalk/tree/v4.1.2
 
 (async () => {
 
-    console.log(`taskText = ${taskText}`);
 
     
     const cookiesString = fs.readFileSync(userSession, 'utf8');
@@ -42,22 +41,24 @@ const showDate = true; //todo
     await page.goto(url, {waitUntil: 'networkidle0'});
     // console.info("url is ", url);
 
-    if(showDate === 'true') {
-      const date = moment().format("ddd Do MMM h:mma"); 
-      task = `${taskText} *(${date})*`
+    const data = await page.evaluate(() => [...document.querySelectorAll(".todo--current .todo__body .todo__list-item.draggable .todo-content__text")].map((i)=>i.textContent));
+
+    
+    const tasks = data.map((t,i) => ([t.trim()])); 
+    const header = [
+    {
+      value: "task",
+      color: "red",
+      align: "left",
+      width: 200,
+    }];
+    const options = {
+      width: "80%"
     }
-    else {
-      task = taskText
-    }
-
-
-    await page.type('.todo--current .todo__body .todo__input', task);
-    await page.keyboard.press("Enter");
-
-    // const data = await page.evaluate(() => [...document.querySelectorAll('.todo--current .todo__body .todo__input')].map(i => console.log(i.outerHTML)));
-    const data = await page.evaluate(() => [...document.querySelectorAll(".todo--current .todo__body .todo__list-item:not(.is-done):not(.is-editing) .todo-content__text")].pop().textContent);
-
-    console.log(`Successfully added new todo "${task}"`)
+    const table = new Table(header, tasks, options);
+    console.log(table.render());
+    
+    // console.log(`Successfully added new todo "${task}"`)
 
     browser.close();
 
---
+++ cli/td.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# https://www.shellcheck.net/
+# usage: 
+# ./td.sh "text" - adds task to today list in teuxdeux
+# create aliases in .zshrc
+# 
+
+if [ $# -ne 1 ]; then
+        echo "🤦‍♀️ You need to actually write something!"
+		exit 1
+fi
+
+taskText=$1
+
+
+node cli/index.js $taskText
+
+# if [ ! -z "$taskText"  ]; then 
+#     echo "$taskText" >> "$DRAFTS_INBOX_LOCATION"/test.md
+#     echo "🎉  Draft created (I hope)"
+#     open /Applications/Drafts.app 
+# else 
+#     echo "🤦‍♀️ Trying to add an empty string"
+# fi
